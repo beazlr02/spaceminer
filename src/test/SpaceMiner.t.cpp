89b88e6b3d9b81fd651915d63a8ec41c20cd3040
beazlr02 <ross.beazley@bbc.co.uk>
2019-07-20 08:29:59 +0100

    Modify test to assert on value of space miner

--- src/test/SpaceMiner.t.cpp
+++ src/test/SpaceMiner.t.cpp
@@ -1,10 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "../main/SpaceMiner.h"
 
 TEST_CASE("Hello", "[World]" ) {
 
     SECTION("A message aboot test") {
-        int s = 42;
-        REQUIRE( s == 42);
+    
+        SpaceMiner spc;
+        int s = spc.speed();
+    REQUIRE( s == 42);
     }
 }
