78c700fae89ead20cc04b6ac7662d430cbf6b010
beazlr02 <ross.beazley@bbc.co.uk>
2019-08-30 15:08:04 +0100

    Add parsing of engine commands to engine pipe adapter

--- src/test/EnginePipeAdapter.t.cpp
+++ src/test/EnginePipeAdapter.t.cpp
@@ -52,31 +52,56 @@ EnginePipeAdapter::EnginePipeAdapter(ThrottlePostionFactory tpf, EngineTrait* en
 
 void EnginePipeAdapter::tick()
 {
+    std::string HIGH = "high\n";
+    std::string MEDIUM = "medium\n";
+    std::string LOW = "low\n";
+    std::string line = iPipe->line();
+    if(line == HIGH) {
     this->engineTrait->engine(this->tpf.high());
+    } else if(line == MEDIUM) {
+        this->engineTrait->engine(this->tpf.medium());
+    } else if(line == LOW) {
+        this->engineTrait->engine(this->tpf.low());
+    } else {
+        this->engineTrait->engine(this->tpf.closed());
+    }
 }
 
-/**
- * closed() {return ThrottlePostion(0);}
-        ThrottlePostion low() {return ThrottlePostion(lowValue);}
-        ThrottlePostion medium() {return ThrottlePostion(mediumValue);}
-        ThrottlePostion high(
- */
-TEST_CASE("Engine set to full" ) {
+TEST_CASE("Parsing messages from the pipe" ) {
 
-    ThrottlePostion expected{20};
     EngineTraitCaptor captor;
-
     InputPipe iPipe{};
-    iPipe.lineToRead = "high\n";
     ThrottlePostionFactory tpf{10,15,20};
-
-
-
     EnginePipeAdapter pipeAdapter{tpf, &captor, &iPipe};
 
-    pipeAdapter.tick();
+    
+    SECTION("high") {
+        ThrottlePostion expected{20};
+        iPipe.lineToRead = "high\n";
+        pipeAdapter.tick();
+        REQUIRE( captor.throttlePosition.thrustInNewtons == expected.thrustInNewtons);
+    }
 
-    SECTION("Initial speed") {
+
+    SECTION("medium") {
+        ThrottlePostion expected{15};
+        iPipe.lineToRead = "medium\n";
+        pipeAdapter.tick();
+        REQUIRE( captor.throttlePosition.thrustInNewtons == expected.thrustInNewtons);
+    }
+
+    SECTION("low") {
+        ThrottlePostion expected{10};
+        iPipe.lineToRead = "low\n";
+        pipeAdapter.tick();
+        REQUIRE( captor.throttlePosition.thrustInNewtons == expected.thrustInNewtons);
+    }
+
+
+    SECTION("closed") {
+        ThrottlePostion expected{0};
+        iPipe.lineToRead = "closed\n";
+        pipeAdapter.tick();
         REQUIRE( captor.throttlePosition.thrustInNewtons == expected.thrustInNewtons);
     }
 }
